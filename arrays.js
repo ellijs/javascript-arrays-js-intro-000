1aa96478963816a8d0aa21877a226620bf706743
ji sun lee <ellijs@hotmail.com>
2021-03-23 03:02:21 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+function addElementToBeggingOfArray(array, element) {
+  const newArray = [...array, element];
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.push(element);
+}
