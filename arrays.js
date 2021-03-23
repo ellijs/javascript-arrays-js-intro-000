b93457821eca3faa2286e3cdb592fdc30eb227e4
ji sun lee <ellijs@hotmail.com>
2021-03-23 03:57:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,8 +16,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push();
-  return array;
+  return array.push();
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
